0278a15a243925ee50ded827eb1d026514c18676
Julia <juliapodobr@gmail.com>
2019-02-21 21:03:25 +0200

    fetch request

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const loadContent = async (url, callback) => {
+    await fetch(url)
+      .then(response => response.json())
+      .then(json => createElement(json.goods));
+  
+    callback();
+  }
+  
+  function createElement(arr) {
+    const goodsWrapper = document.querySelector('.goods__wrapper');
+  
+    arr.forEach(item => {
+      let card = document.createElement('div');
+      card.classList.add('goods__item');
+      card.innerHTML = `
+        <img class="goods__img" src="${item.url}" alt="phone">
+        <div class="goods__colors">Доступно цветов: 4</div>
+        <div class="goods__title">
+          ${item.title}
+        </div>
+        <div class="goods__price">
+          <span>${item.price}</span> руб/шт
+        </div>
+        <button class="goods__btn">Добавить в корзину</button>
+      `;
+      goodsWrapper.appendChild(card);
+    });
+  }
 
+  loadContent('js/db.json', () => {
     const cartWrapper = document.querySelector('.cart__wrapper'),
     cart = document.querySelector('.cart'),
     close = document.querySelector('.cart__close'),
@@ -10,48 +39,48 @@ window.addEventListener('DOMContentLoaded', () => {
     badge = document.querySelector('.nav__badge'),
     totalCost = document.querySelector('.cart__total > span'),
     titles = document.querySelectorAll('.goods__title');
-
+  
     function openCart() {
     cart.style.display = 'block';
     document.body.style.overflow = 'hidden';
     }
-
+  
     function closeCart() {
     cart.style.display = 'none';
     document.body.style.overflow = '';
     }
-
+  
     open.addEventListener('click', openCart);
     close.addEventListener('click', closeCart);
-
+  
     goodsBtn.forEach((btn, i) => {
       btn.addEventListener('click', () => {
         let item = products[i].cloneNode(true),
             trigger = item.querySelector('button'),
             removeBtn = document.createElement('div'),
             empty = cartWrapper.querySelector('.empty');
-
+  
         trigger.remove();
-
+  
         showConfirm();
         calcGoods(1);
-
+  
         removeBtn.classList.add('goods__item-remove');
         removeBtn.innerHTML = '&times';
         item.appendChild(removeBtn);
-
+  
         cartWrapper.appendChild(item);
-
+  
         if (empty) {
             empty.style.display = 'none';
         }
-
+  
         calcTotal();
         removeFromCart();
-
+  
       });
     });
-
+  
     function sliceTitle() { 
       titles.forEach((item) => {
       if (item.textContent.length < 70) {
@@ -63,7 +92,7 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
     sliceTitle();
-
+  
     function showConfirm() {
       confirm.style.display = 'block';
       let counter = 100;
@@ -79,17 +108,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       }
     }
-
+  
     function calcGoods(i) {
       const items = cartWrapper.querySelectorAll('.goods__item');
       badge.textContent = i + items.length;
       const empty = cartWrapper.querySelector('.empty');
-
+  
       if (items.length === 0) {
         empty.style.display = 'block';
       }
     }
-
+  
     function calcTotal() {
       const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
       let total = 0;
@@ -98,10 +127,10 @@ window.addEventListener('DOMContentLoaded', () => {
       });
       totalCost.textContent = total;
     }
-
+  
     function removeFromCart() {
       const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
-
+  
       removeBtn.forEach((btn)=> {
         btn.addEventListener('click', () => {
           btn.parentElement.remove();
@@ -110,7 +139,6 @@ window.addEventListener('DOMContentLoaded', () => {
         })
       });
     }
-
-})
-
+  });
+});
 
